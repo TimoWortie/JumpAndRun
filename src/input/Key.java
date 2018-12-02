f1fc91412453cd34ec5a70d123e9a506514b6f62
Carina96 <45451715+Carina96@users.noreply.github.com>
2018-12-02 22:48:48 +0100

    Update Key.java

--- src/input/Key.java
+++ src/input/Key.java
@@ -5,21 +5,62 @@ import java.awt.event.KeyListener;
 
 public class Key implements KeyListener {
 
-	@Override
+	
 	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		if(!Game.launcher.launching){
+			int key = e.getKeyCode();
+			for(Entity en:Game.handler.entity){
+				if(en.tot==false){
+			if(key== KeyEvent.VK_D){
+				d = true;
+			}
+			if(key== KeyEvent.VK_A){
+				a=true;
+			}
+			if(key== KeyEvent.VK_W){
+				if(!en.jumping&&!en.falling&&en.getId()!=Id.Player&&Game.player.getStunned()==false){
+				if(!en.jumping&&!en.falling){
+					Game.handler.ChangeMusic(4, 1, false);
+					en.jumping=true;
+					en.gravity=17.0f;
+				}
+				}
+			}
+			
+			if(key== KeyEvent.VK_ESCAPE){
+				Game.launcher.launching=true;
+			}
+
+			if(key== KeyEvent.VK_SHIFT){
+				 shift = true;
+			}}}}
 	}
 
-	@Override
+	
 	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
+		if(!Game.launcher.launching){
+		int key = e.getKeyCode();
+		for(Entity en:Game.handler.entity){
+		if(key== KeyEvent.VK_D){
+			Game.player.setVelX(0);
+			Player.moving=-1;
+			d=false;
+		}
+		if(key==e.VK_A){
+			Game.player.setVelX(0);
+			Player.moving=-2;
+			a=false;
+		}
+		if(key==KeyEvent.VK_SHIFT){
+			shift = false;
+		}
+	  }
 		
-	}
+	}}
 
-	@Override
+	
 	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
