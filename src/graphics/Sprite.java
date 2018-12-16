bd311ca1b6409604972999265ac9c61c6fa81123
Carina96 <45451715+Carina96@users.noreply.github.com>
2018-12-16 21:04:13 +0100

    Update Sprite.java

--- src/graphics/Sprite.java
+++ src/graphics/Sprite.java
@@ -1,8 +1,20 @@
 package graphics;
 
 import java.awt.image.BufferedImage;
-
 import graphics.Spritesheet;
+
+/**
+ * Creator of the Spritesheet.
+ * 
+ * @author Carina Fenn
+ * @since 14.12.2018
+ * @version 1.0
+ */
+
+	/**
+	 * This class can either load an image file or it can load a sprite from a sprite sheet.
+	 * 
+	 */
 
 public class Sprite {
 
